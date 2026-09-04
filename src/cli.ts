0e54e4dbcc9cae2bf0a414b2679ce57b9a996ce7
logandavies181 <logan.davies181@gmail.com>
2026-09-04 21:56:57 +1200

    render from bar number

--- src/cli.ts
+++ src/cli.ts
@@ -6,12 +6,27 @@ export async function run(builder: Builder): Promise<void> {
     .name("tsesound")
     .version("0.1.0")
     .description("Experimental command line interface for TseSound")
+    .globalOption(
+      "--from-bar <bar:number>",
+      "Bar to start from (0-indexed)",
+      {
+        default: 0,
+      },
+    )
     .command("perform", "Render CSound code and immediately perform it.")
     .option("-o, --output <val:string>", "Output file", {
       default: "dac",
     })
+    .option(
+      "--from-bar <bar:number>",
+      "Bar to start from (0-indexed)",
+      {
+        default: 0,
+      },
+    )
     .action((options) => {
-      const csdText = builder.render()
+      const fromBar = (options.fromBar as number) ?? 0
+      const csdText = builder.render(fromBar)
       // TODO: write .csd to a temp file.
       Deno.writeTextFileSync("gen.csd", csdText) // TODO: only use .exe on windows, and allow overriding.
        // execFileSync("csound.exe", ["gen.csd", "-o", options.output])
